bbe2034ac2b3dcd591d1337497885a1cb6b6630c
Atalya Alon <atalya.alon@gmail.com>
2020-04-12 01:16:54 +0300

    install script: update installations, add cuda installation

--- scripts/install_python_and_download_datasets.sh
+++ scripts/install_python_and_download_datasets.sh
@@ -7,6 +7,7 @@ sudo apt-get install python3-setuptools;
 sudo apt-get update && sudo apt install python3-pip;
 sudo python3 setup.py install;
 sudo pip3 install pycocotools matplotlib;
+sudo apt-get install freeglut3 freeglut3-dev libxi-dev libxmu-dev;
 mkdir openpifpaf/outputs;
 mkdir datasets; cd datasets;
 mkdir "data-mscoco";
@@ -21,4 +22,6 @@ wget http://images.cocodataset.org/zips/val2017.zip;
 unzip val2017.zip;
 wget http://images.cocodataset.org/zips/train2017.zip;
 unzip train2017.zip;
-
+cd ~;
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run;
+sudo sh cuda_10.2.89_440.33.01_linux.run;
